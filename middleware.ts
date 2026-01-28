8b12dc3e3524d49d2b77076abf01dd7787a7bd9b
peleg1711-boop <pelegmv@gmail.com>
2026-01-28 17:07:08 +0200

    Fix landing page public route

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/landing"],  // Make root public
+  publicRoutes: ["/landing"],
 });
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-  publicRoutes: ["/landing"],
 };
